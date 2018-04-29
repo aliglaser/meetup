247c03d4e129f7769dc692d918d6e56683014550
Rockheung <rockheung@gmail.com>
2018-04-29 14:54:50 +0900

    add teaming app

--- _base/teaming/models.py
+++ _base/teaming/models.py
@@ -1,3 +1,11 @@
+import datetime
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Room( models.Model ):
+    pass
+
+class TeamingUser( User ):
+    pass
