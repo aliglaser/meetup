59eb89f4e659b9252c3d35d49edd402af64c7dec
Rockheung <rockheung@gmail.com>
2018-05-01 18:51:20 +0900

    dev merge

--- _base/teaming/models.py
+++ _base/teaming/models.py
@@ -1,3 +1,18 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Room( models.Model ):
+    id =models.PositiveIntegerField()
+    users =models.ManyToManyField(User)
+    user_limit =models.PositiveIntegerField()
+    tags =models.CharField( max_length =256)
+    age_limit =models.CharField( max_length =16 )
+    date =models.CharField( max_length =64 )
+    location =models.CharField( max_lenth =128 )
+
+
+class TeamingUser( User ):
+    pass
