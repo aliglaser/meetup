dba0ad78a19305d9eee1909c3318e25728069c67
Rockheung <rockheung@gmail.com>
2018-05-03 21:51:36 +0900

    working dev

--- _base/teaming/routing.py
+++ _base/teaming/routing.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/teaming/(?P<room_name>[^/]+)/$', consumers.TeamingConsumer),
+    url(r'^ws/teaming/(?P<room_name>[^/]+)/$', consumers.AsyncTeamingConsumer),
 ]
